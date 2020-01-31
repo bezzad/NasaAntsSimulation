d4494e33a8a5ff684c61add97e0fdf61bfb79830
Behzad <behzad.khosravifar@gmail.com>
2020-01-31 16:08:26 +0330

    refactoring

--- src/NasaSimulation/Core/FaultGenerator.cs
+++ src/NasaSimulation/Core/FaultGenerator.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Simulation.Enums;
+﻿using Simulation.Enums;
 using Simulation.Tools;
+using System.Linq;
 
 namespace Simulation.Core
 {
